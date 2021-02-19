64495d2f233c0e1d9146853b2598fe27e9afeab5
“zed” <zed@kolk.me>
2021-02-18 23:16:59 -0600

    tried to get a user#show page working, its not there yet

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,10 @@ class UsersController < ApplicationController
         end
     end
 
+    def show
+        @user = User.find_by(params[:id])
+    end
+
 
     private
     def strong_params
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
-user1 = User.create(username: "khalin", password: "password", role: "Owner")
-user2 = User.create(username: "khalin", password: "password", role: "General Contractor")
-user3 = User.create(username: "zed", password: "password", role: "Owner")
+user1 = User.create(username: "khalin", password: "password123", role: "Owner")
+user2 = User.create(username: "bigbird", password: "password456", role: "General Contractor")
+user3 = User.create(username: "zed", password: "mypassword", role: "Subcontractor")
 
 contract1 = Contract.create(title: "Marriot", terms: "completed by the end of the year", user_id: user1.id)
 sub_contract1 = SubContract.create(title: contract1.title, terms: "need plumbers and electricians", user_id: user2.id)
